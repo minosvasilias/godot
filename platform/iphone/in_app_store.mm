bb32077de52156bfae4b9c6d1983db6a44e318a7
minosvasilias <minosvasilias@gmail.com>
2020-12-07 18:15:58 +0100

    -receipt fixes

--- platform/iphone/in_app_store.mm
+++ platform/iphone/in_app_store.mm
@@ -390,7 +390,8 @@ Dictionary InAppStore::get_receipt(){
 	bool sandbox = url_string.find("sandboxReceipt") >= 0;
 	print_line(String("appstorereceipturl: ") + url_string + ", sandbox: " + Variant(sandbox));
 	data = [NSData dataWithContentsOfURL:url];
-	NSString *receipt = [data base64EncodedStringWithOptions:0];
+	//NSString *receipt = [data base64EncodedStringWithOptions:0];
+	NSString *receipt = [self base64forData:data];
 
 	Dictionary res;
 	res["receipt"] = receipt;
